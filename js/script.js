fc5cfbd3b75947360519cc336f65cc4d95894086
WallisonFer <wallisonsp.santos@gmail.com>
2024-05-20 16:04:12 -0300

    Project done

---
+++ js/script.js
@@ -0,0 +1,92 @@
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+const urlTg = "http://s.technogym.com/groupcycle/rower?et=";
+
+
+
+
+
+
+// Button submit
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+  const url = document.getElementById("url").value;
+  const finalUrl = urlTg+url;
+  const size = document.getElementById("size").value;
+
+  // Validate url
+  if (finalUrl === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner();
+    // Show spinner for 1 sec
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(finalUrl, size);
+      showScanner();
+      // Generate the save button after the qr code image src is ready
+      setTimeout(() => {
+        // Get save url
+        const saveUrl = qr.querySelector("canvas").toDataURL();
+        // Create save button
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+};
+
+// Generate QR code
+const generateQRCode = (finalUrl, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: finalUrl,
+    width: size,
+    height: size,
+  });
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+// hide  scanner
+const showScanner = () => {
+  const scanner = document.getElementById("qrCodeContainer");
+  scanner.style.display = "block";
+};
+
+// Show spinner
+const showSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+};
+
+// Hide spinner
+const hideSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+};
+
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.innerHTML = "Save Image";
+
+  link.href = saveUrl;
+  link.download = "qrcode.png";
+
+  document.getElementById("generated").appendChild(link);
+};
+
+hideSpinner();
+
+form.addEventListener("submit", onGenerateSubmit);
